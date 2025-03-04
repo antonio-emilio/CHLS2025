6eff97f2a5b4652a90b003b982a401a04a02100a
Antonio Emilio <antonio_pereira@outlook.com>
2025-03-04 13:13:26 +0100

    Update sink.cpp

--- Mini_projet_FFT/FFT_FIFO_FLOAT_STUDENT_BCA/sink.cpp
+++ Mini_projet_FFT/FFT_FIFO_FLOAT_STUDENT_BCA/sink.cpp
@@ -8,46 +8,28 @@ using std::endl;
 
 void SINK::COMPORTEMENT()
 {
-	std::ofstream realStream("out_real.txt");
-	std::ofstream imagStream("out_imag.txt");
+    std::ofstream realStream("out_real.txt");
+    std::ofstream imagStream("out_imag.txt");
 
-	std::ifstream realStream_ref("out_real_ref_valid.txt");
-	std::ifstream imagStream_ref("out_imag_ref_valid.txt");
+    if (!realStream || !imagStream) {
+        cout << "[SINK] Erro ao abrir arquivos de saída!" << endl;
+        return;
+    }
 
-	float real, imag, real_ref, imag_ref, diff, max_error = 0.;
+    while (true)
+    {
+        if (data_valid.read()) {
+            float real = in.read();
+            float imag = in.read();
 
-	if (!realStream || !imagStream || !realStream_ref || !imagStream_ref)
-		cout << "[SINK] " << "Un des fichiers d'entree n'est pas ouvert" << endl;
-	
-	data_req.write(true);
+            realStream << real << std::endl;
+            imagStream << imag << std::endl;
 
-	while (true)
-	{
-		if (data_valid.read())
-		{
-			cout << "[SINK] " << "Ecriture des fichiers de sortie." << endl;
-			data_req.write(false);
+            data_req.write(true);
+        } else {
+            data_req.write(false);
+        }
 
-			real = in.read();
-			imag = in.read();
-
-			realStream << real << std::endl;
-			imagStream << imag << std::endl;
-
-			realStream_ref >> real_ref;
-			imagStream_ref >> imag_ref;
-
-			diff = std::abs(real - real_ref) + std::abs(imag - imag_ref);
-			if (diff > max_error)
-			{
-				max_error = diff;
-				cout << "[SINK] " << "Erreur maximale actuelle: " << max_error << endl;
-			}
-
-			data_req.write(true);
-		}
-
-		wait();
-	}
-
+        wait();
+    }
 }
